1076841087aa8d927d1af2a9701f33b2148ca31a
Vlad Pavlyuchenko <vlad.pavl@icloud.com>
2020-10-25 00:40:54 +0300

    editing webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,42 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    plugins: [ 
-        new webpack.ProvidePlugin({ 
-            "jQuery": "jquery", 
-            "window.jQuery": "jquery", 
-            "jquery": "jquery", 
-            "window.jquery": "jquery", 
-            "$": "jquery", 
-            "window.$": "jquery" 
-        }) 
+    plugins: [
+        new webpack.ProvidePlugin({
+            "jQuery": "jquery",
+            "window.jQuery": "jquery",
+            "jquery": "jquery",
+            "window.jquery": "jquery",
+            "$": "jquery",
+            "window.$": "jquery"
+        })
     ],
-    
+
     entry: {
         main: "./app/js/index.js",
     },
 
+    output: {
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        publicPath: "/"
+    },
+
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: require.resolve("babel-loader"),
-                    query: {
-                        presets: [
-                            ["@babel/preset-env", { modules: false }]
-                        ]
-                    }
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: require.resolve("babel-loader"),
+                query: {
+                    presets: [
+                        ["@babel/preset-env", {
+                            modules: false
+                        }]
+                    ]
                 }
             }
-        ]
+        }]
     },
 
     resolve: {
